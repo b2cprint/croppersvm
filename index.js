4a4db4c570207756fc51f485c9b8d65deccaf764
Shahaf Antwarg <santwarg@gmail.com>
2018-02-14 10:50:35 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+export function specialViewMode(canvas, cropBox) {
+    var newCanvasData = {};
+    var cropBoxAspectRatio = cropBox.width / cropBox.height;
+    var cropBoxRight = cropBox.left + cropBox.width;
+    var cropBoxBottom = cropBox.top + cropBox.height;
+    var canvasAspectRatio = canvas.width / canvas.height;
+    var canvasRight = canvas.left + canvas.width;
+    var canvasBottom = canvas.top + canvas.height;
+    var checkHorizontalBorders = function (isBigger) {
+        if (compare(cropBox.left, canvas.left, isBigger)) {
+            newCanvasData.left = cropBox.left;
+        } else if (compare(canvasRight, cropBoxRight, isBigger)) {
+            newCanvasData.left = cropBoxRight - canvas.width;
+        }
+    };
+    var checkVerticalBorders = function (isBigger) {
+        if (compare(cropBox.top, canvas.top, isBigger)) {
+            newCanvasData.top = cropBox.top;
+        } else if (compare(canvasBottom, cropBoxBottom, isBigger)) {
+            newCanvasData.top = cropBoxBottom - canvas.height;
+        }
+    };
+    var checkNewWidth = function (newCanvasWidth) {
+        if (newCanvasWidth !== canvas.width) {
+            newCanvasData.width = newCanvasWidth;
+        }
+    };
+    var options = {
+        width: Math.max(canvas.width, cropBox.width),
+        horizontal: false,
+        vertical: canvas.height < cropBox.height
+    };
+    if (canvasAspectRatio < cropBoxAspectRatio) {
+        options.width = Math.max(canvas.height, cropBox.height) * canvasAspectRatio;
+        options.horizontal = canvas.width < cropBox.width;
+        options.vertical = false;
+    }
+    checkNewWidth(options.width);
+    checkHorizontalBorders(options.horizontal);
+    checkVerticalBorders(options.vertical);
+    return newCanvasData;
+}
+/**
+ * If the opposite param is set to false then the function will actually be isSmaller.
+ *
+ * @param a
+ * @param b
+ * @param isBigger
+ * @returns {boolean}
+ */
+function compare (a, b, isBigger) {
+    return a > b === isBigger && a !== b;
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+## cropperSVM
+This package is an extension that was created for cropperjs plugin in order to have a new view mode that allows you to limit the cropping options into the inner edges of the cropbox.
+
+It never allows the image to have shorter sides than the cropbox on all four sides in the same moment.
+
+Simple implementation example:
+```
+import { specialViewMode } from 'croppersvm';
+
+var image = document.getElementById('image');
+var cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  viewMode: 0,
+  crop: function(e) {
+    var cropBox = cropper.getCropBoxData();
+    var canvas = cropper.getCanvasData();
+
+    var newCanvasData = specialViewMode(canvas, cropBox);
+
+    if (newCanvasData.width || newCanvasData.left || newCanvasData.top) {
+        cropper.setCanvasData(newCanvasData);
+    }
+  }
+});
+```
